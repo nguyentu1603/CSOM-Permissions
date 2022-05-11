c385e30cdd347c47a6d1a0519ae3260cbcaa10e4
tychuot163 <42094955+tychuot163@users.noreply.github.com>
2022-05-11 13:37:03 +0700

    assignment csom permissions

---
+++ ConsoleCSOM/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SharePoint.Client;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.SharePoint.Client.Taxonomy;
+using ConsoleCSOM.Helpers;
+using System.Collections.Generic;
+using ConsoleCSOM.Models;
+using System.Text;
+using System.IO;
+using File = Microsoft.SharePoint.Client.File;
+using Microsoft.SharePoint.Client.UserProfiles;
+
+namespace ConsoleCSOM
+{
+    class SharepointInfo
+    {
+        public string AdminSiteUrl { get; set; }
+        public string SiteUrl { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                using (var clientContextHelper = new ClientContextHelper())
+                {
+                    ClientContext ctx = GetContext(clientContextHelper);
+                    ctx.Load(ctx.Web);
+                    await ctx.ExecuteQueryAsync();
+
+                    Console.WriteLine($"Site {ctx.Web.Title}");
+                }
+                Console.WriteLine($"Press Any Key To Stop!");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        static ClientContext GetContext(ClientContextHelper clientContextHelper)
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile($"appsettings.json", true, true);
+            IConfiguration config = builder.Build();
+            var info = config.GetSection("SharepointInfo").Get<SharepointInfo>();
+            return clientContextHelper.GetContext(new Uri(info.SiteUrl), info.Username, info.Password);
+        }
+
+    }
+}
